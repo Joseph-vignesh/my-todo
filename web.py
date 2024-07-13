5f98e7348de105598205b92933fece4409d1df15
vignesh <vickyasvickyb@gmail.com>
2024-07-13 16:35:12 +0530

    added todo input text feature and calling it in the text files

--- web.py
+++ web.py
@@ -3,13 +3,16 @@ import function
 
 todos = function.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    function.write_todos(todos)
+
 st.title("My-Todo-App")
 st.subheader("Chase your Tasks")
 st.write("This app boosts your productivity")
 
-st.checkbox("Lets go !!!!!")
-for todo in todos:
-    st.checkbox(todo)
-st.checkbox("yayyy")
+for i, todo in enumerate(todos):
+    st.checkbox(todo, key=f"todo_{i}")
 
-st.text_input(label= "A*****Z", placeholder="Add your todo here")
+st.text_input(label="A*****Z", placeholder="Add your todo here", on_change=add_todo, key='new_todo')
